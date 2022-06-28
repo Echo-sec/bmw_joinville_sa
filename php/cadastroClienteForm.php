4aea67b8cdbf26227051a1e292f176cc209a03bc
Eduardo Alexandre <eduardoa.m.hermes@gmail.com>
2022-06-28 19:47:12 -0300

    cadastro cliente

--- php/cadastroClienteForm.php
+++ php/cadastroClienteForm.php
@@ -4,42 +4,58 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Cadastro de clientes</title>
   </head>
   <body>
+
+
+  <div id="alertas">
+    <?php if(isset($_GET['retorno'])==true && $_GET['retorno']==0){ ?>
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <span>Houve algum problema ao a cadastrar o cliente!</span>
+        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+    </div>
+    <?php }else if(isset($_GET['retorno'])==true && $_GET['retorno']==1) ?>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <span>Cliente cadastrado com sucesso!</span>
+        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+    </div>
+
+    
+</div>
       
     <div class="container" >
       <div ></div>
         <div class="py-5 text-left">
-          <img class="d-block mx-auto mb-4" src="imgLogin/Logo BMW.png" alt="" width="90" height="90">
+          <img class="d-block mx-auto mb-4" src="../imgLogin/Logo BMW.png" alt="" width="90" height="90">
           <h2>Formulário de Cadastro</h2>
         </div>
   
         <div class="row">
 
           <div class="col-md-12 order-md-1">
-            <form class="needs-validation" novalidate>
+            <form class="needs-validation" action="cadastroCliente.php" method="POST" novalidate>
               <div class="row">
                 <div class="col-md-4 mb-3">
                   <label for="primeiroNome">Nome</label>
-                  <input type="text" class="form-control" id="primeiroNome" placeholder="Eduardo Alexandre" value="" required>
+                  <input type="text" class="form-control" name="nome" id="primeiroNome" placeholder="Eduardo Alexandre" value="" required>
                   <div class="invalid-feedback">
                     É obrigatório inserir um nome válido.
                   </div>
                 </div>
                 <div class="col-md-4 mb-3">
                   <label for="sobrenome">CPF</label>
-                  <input type="number" class="form-control" id="CPF" placeholder="XXX.XXX.XXX-XX" value="" required>
+                  <input type="number" class="form-control" name="cpf" id="CPF" placeholder="XXX.XXX.XXX-XX" value="" required>
                   <div class="invalid-feedback">
-                    É obrigatório inserir um CPFválido.
+                    É obrigatório inserir um CPF válido.
                   </div>
                 </div>
                 <div class="col-md-4 mb-3">
                   <label for="sobrenome">Data de nascimento</label>
-                  <input type="date" class="form-control" id="CPF"  value="" required>
+                  <input type="date" class="form-control" name="dataNasc" id="CPF"  value="" required>
                   <div class="invalid-feedback">
-                    É obrigatório inserir um CPFválido.
+                    É obrigatório inserir uma data de nascimento.
                   </div>
                 </div>
               </div>
@@ -47,26 +63,25 @@
               <div class="mb-3">
                 <label for="nickname">CEP</label>
                 <div class="input-group">
-                  <input type="text" class="form-control" id="CEP" placeholder="CEP" required>
+                  <input type="text" class="form-control" id="CEP" name="cep" placeholder="CEP" required>
                   <div class="invalid-feedback" style="width: 100%;">
-                    Seu nickname é obrigatório.
+                    Seu CEP é obrigatório.
                   </div>
                 </div>
               </div>
   
               <div class="mb-3">
                 <label for="email">N°</label>
-                <input type="email" class="form-control" id="numeroCasa" placeholder="291">
+                <input type="email" class="form-control" id="numeroCasa" name="numCasa" placeholder="291">
                 <div class="invalid-feedback">
-                  Por favor, insira um endereço de e-mail válido, para atualizações de entrega.
-                </div>
+                  Por favor, insira um número de casa
               </div>
   
               <div class="mb-3">
                 <label for="endereco">Telefone</label>
-                <input type="text" class="form-control" id="telefone" placeholder="99 9999-9999" required>
+                <input type="text" class="form-control" id="telefone" name="telefone" placeholder="99 9999-9999" required>
                 <div class="invalid-feedback">
-                  Por favor, insira seu endereço de entrega.
+                  Por favor, insira seu telefone
                 </div>
               </div>
                 
