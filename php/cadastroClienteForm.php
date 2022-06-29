0a4093db791e199f0fe58d4627cdda1af0ef0c1a
Eduardo Alexandre <eduardoa.m.hermes@gmail.com>
2022-06-28 21:09:34 -0300

    atualização cadastro v2

--- php/cadastroClienteForm.php
+++ php/cadastroClienteForm.php
@@ -16,11 +16,12 @@
     <span>Houve algum problema ao a cadastrar o cliente!</span>
     <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
   </div>
-  <?php }else if(isset($_GET['retorno'])==true && $_GET['retorno']==1) ?>
+  <?php }else if(isset($_GET['retorno'])==true && $_GET['retorno']==1) {?>
   <div class="alert alert-success alert-dismissible fade show" role="alert">
     <span>Cliente cadastrado com sucesso!</span>
     <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
   </div>
+  <?php } ?>
 
 
 </div>
@@ -147,7 +148,7 @@
           <tr>
             <?php
                 require_once("conexaoBanco.php");
-                $comando="SELECT nomeCompleto, cpf, dataNascimento from clientes";
+                $comando="SELECT idCliente, nomeCompleto, cpf, dataNascimento from clientes";
                  if(isset($_GET['pesquisa']) && $_GET['pesquisa']!=""){
                      $comando.=" WHERE nomeCompleto LIKE '".$_GET['pesquisa']."%'";
                  }
@@ -168,7 +169,26 @@
                   echo "<td>".date('d/m/Y', strtotime($c['dataNascimento']))."</td>";
          ?>
 
-          </tr>
+          
+              <td>
+          <form action="editarClienteForm.php" method="POST" class="formAcao">
+            <input type="hidden" name="idComp" value="<?=$c['idCliente']?>">
+            <button type="submit" class="botaoAcao">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+              </svg>
+            </button>				
+          </form>
+          <form action="excluirCliente.php" method="POST" class="formAcao">
+            <input type="hidden" name="idComp" value="<?=$c['idCliente']?>">
+            <button type="submit" class="botaoAcao">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+              <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+              </svg>
+            </button>				
+          </form>
+          </td>
+              </tr>
           <?php } //fechamento foreach
                 } //fechamento else
                 ?>
@@ -176,25 +196,6 @@
         </table>
       </div>
     </div>
-    <!-- <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                  </tr>
-                </tbody> -->
     </table>
     <hr class="mb-4">
 
