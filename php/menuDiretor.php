8f5ff162cee25f47b4c5050ee2da69b0a9d103af
Eduardo Alexandre <eduardoa.m.hermes@gmail.com>
2022-06-28 21:41:04 -0300

    atualizações final dia

--- php/menuDiretor.php
+++ php/menuDiretor.php
@@ -1,24 +1,28 @@
-<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #DCF2D2;">
-  <div class="container-fluid">   
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Agendamentos
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="consultarCompromissoDiretor.php">Consultar</a></li>
-          </ul>
-        </li>  
-             
-      </ul>
-      <div class="d-flex">    
-       <a href="efetuarLogout.php"> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
-            <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
-        </svg>
-    </a>
+<header>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="../imgLogin/Logo Bmw.png" alt="..." height="55">
+        Bmw Joinville
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Menu
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Consulta de agenamentos</a></li>
+              <li><a class="dropdown-item" href="#">Consulta dos fechamentos</a></li>
+              <li><a class="dropdown-item" href="#">Relatório de vendas</a></li>
+              <li><a class="dropdown-item" href="cadastroUsuarioForm.php">Cadastro de usuários</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
-    </div>
-  </div>
-</nav>
+  </nav>
+</header>
