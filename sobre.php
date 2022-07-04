baafa3cb81dbd687c87cb22db71880676f869a10
Lucas Mioti <LucasMioti10@gmail.com>
2022-07-04 19:51:37 -0300

    att

---
+++ sobre.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="webSite/CSS_WebSite/sobre.css">
+    <title>Sobre</title>
+</head>
+<body>
+<header>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="imgLogin/Logo Bmw.png" alt="..." height="55">
+        Bmw Joinville
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Menu
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Agendamentos de visita</a></li>
+              <li><a class="dropdown-item" href="#">Consultas de agendamentos</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/car-card.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
+
+<div class="py-5 px-5">
+    <div class="text-center pb-5">
+        <h1><strong>Sobre nós</strong></h1>
+    </div>  
+    <div class="">
+        <h4>A BMW Joinville atua no mercado de revenda automotiva para fazer a diferença.</h4>
+        <h4>Honestidade, rigor e procedência sustentam nosso minucioso critério de avaliação e comercialização de veículos.</h4>
+        <h4>Surpreenda-se com a fórmula da BMW de fazer negócio.</h4>
+        <h4>Unimos nossa paixão por carros com sua paixão por dirigir.</h4>
+    </div>
+</div>
+
+<!-- sections avisos -->
+<section class="bg-gradient py-5">
+    <div class="row">
+        <div class="col-sm-12 text-white text-center my-3">
+            <h1>Disponibilidade</h1>
+        </div>
+    </div>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-sm-12 bg-dark text-white py-3">
+                <div class="accordion" id="accordionPanelsStayOpenExample">
+                    <div class="accordion-item text-white bg-dark">
+                        <h2 class="accordion-header text-white" id="panelsStayOpen-headingOne">
+                        <button class="accordion-button text-white bg-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                            Horário de funcionamento
+                        </button>
+                        </h2>
+                        <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                        <div class="accordion-body">
+                            <strong>Funcionamos todos os dias.</strong>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+</section>
+
+<!-- footer -->
+<footer>
+    <div class="container-fluid bg-gradient">
+        <div class="row">
+            <div class="col-sm-12 text-white py-3">
+                <p class="mb-0">Copyright &copy; 2021 - Todos os direitos reservados para <strong>BMW Joinville</strong>.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
