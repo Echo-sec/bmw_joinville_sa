ba979957afb9fb03bbd310b1445ff83265c704e1
Eduardo Alexandre <eduardoa.m.hermes@gmail.com>
2022-07-06 19:32:22 -0300

    atualizações tela

--- php/consultaFechamentoForm.php
+++ php/consultaFechamentoForm.php
@@ -7,7 +7,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="../CSS/telaAgendamento.css">
-  <title>Consulta de Vendas</title>
+  <title>Consulta de Fechamentos</title>
 </head>
 
 <body>
@@ -17,7 +17,7 @@
     <div></div>
     <div class="py-5 text-left text-white">
       <img class="d-block mx-auto mb-3" src="../imgLogin/Logo BMW.png" alt="" width="90" height="90">
-      <h2>Consulta de Vendas</h2>
+      <h2>Consulta de Fechamentos</h2>
     </div>
 
     <div class="row">
@@ -43,17 +43,10 @@
               <label for="date">Escolha uma data:</label>
               <input type="date" id="data" class="form-control" required>
             </div>
-
-            <div class="form-row">
-              <div class="form-group col-md-4" style="width: 100%;">
-                <label for="nomeFechador">Nome do Fechador:</label>
-                <input type="text" id="nomeFechador" class="form-control" placeholder="João" required>  
-                <br>
-              </div>
-            </div>
+            
         </form>
         <div>
-            <button class="btn btn-primary" type="submit">Pesquisar</button>
+            <button class="btn btn-primary" name="pesquisa" type="submit">Pesquisar</button>
         </div>
        
             <div>
@@ -64,13 +57,41 @@
                     <th scope="col">Carro</th>
                     <th scope="col">Cliente</th>
                     <th scope="col">Data</th>
-                    <th scope="col">Hora</th>
-                    <th scope="col">Status</th>
+                    <th scope="col">Valor</th>
                     <th scope="col">Vendedor</th>
                   </tr>
                 </thead>
-                <tbody> 
                 <tr>
+            <?php
+                require_once("conexaoBanco.php");
+                $comando="SELECT * from fechamentos";
+                 if(isset($_GET['pesquisa']) && $_GET['pesquisa']!=""){
+                     $comando.=" WHERE nomeCompleto LIKE '".$_GET['pesquisa']."%'";
+                 }
+                 $resultado=mysqli_query($conexao, $comando);
+                 $fechamento=array();
+                 $linhas=mysqli_num_rows($resultado);
+                 if($linhas==0){
+                  echo "<tr><td colspan='6'>Nenhum Fechamento encontrado</td></tr>";
+                      }else{
+                          while($c = mysqli_fetch_assoc($resultado)){
+                                array_push($fechamento, $c);
+                        }
+
+                      foreach($clientes as $c){
+                          echo "<td>".$c['nomeCompleto']."</td>";
+                          echo "<td>".$c['cpf']."</td>";
+                          echo "<td>".date('d/m/Y', strtotime($c['dataNascimento']))."</td>";
+                         ?>
+
+          
+              <td>
+
+                      </tr>
+                      </td>
+                      <?php } //fechamento foreach
+                } //fechamento else
+                ?>
               </table>
             </div>
         </div>
